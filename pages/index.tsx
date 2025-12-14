877e452350474d0a6a71c53d74ca9d4ba8f45317
AndrianPOPA <andrianpopa.r@gmail.com>
2025-12-14 14:54:21 +0200

    Fix: Add header and footer to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import HeaderOne from "@/components/headers/HeaderOne";
 import HomePageOne from "@/components/pages/home-pages/HomePageOne";
+import Footer from "@/components/footers/Footer";
 
 export default function Home() {
-  return <HomePageOne />;
+  return (
+    <>
+      <HeaderOne />
+      <HomePageOne />
+      <Footer />
+    </>
+  );
 }
